bc3134fa03c30ba0a361d2034b1c12790df6aa15
kdgreen58 <kolton.green@spsmail.cuny.edu>
2014-11-06 21:20:46 -0500

    My code for task 01

--- task_01.py
+++ task_01.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 """My task 01 code."""
 
-GRADE_DATA = {}
-GRADE_DATA['student'] = {'Luke Skywalker', 'Luke Skywalker', 'Luke Skywalker',
+GRADE_DATA = {'student':{'Luke Skywalker', 'Luke Skywalker', 'Luke Skywalker',
                          'Luke Skywalker', 'Han Solo','Han Solo', 'Han Solo',
-                         'Han Solo', 'C-3PO', 'C-3PO', 'C-3PO', 'C-3PO'}
-GRADE_DATA['subject'] = {'math', 'etiquette', 'grammar', 'gym', 'math', 
+                         'Han Solo', 'C-3PO', 'C-3PO', 'C-3PO', 'C-3PO'},
+              'subject':{'math', 'etiquette', 'grammar', 'gym', 'math', 
                          'etiquette', 'grammar', 'gym', 'math', 'etiquette', 
-                         'grammar', 'gym'}
-GRADE_DATA['grade'] = {'B', 'B+', 'B', 'A', 'A-', 'C-', 'B', 'B', 'C', 'A+', 
+                         'grammar', 'gym'},
+              'grade':{'B', 'B+', 'B', 'A', 'A-', 'C-', 'B', 'B', 'C', 'A+', 
                        'A', 'F'}
+    }
