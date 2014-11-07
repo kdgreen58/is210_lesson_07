a66fc28609f0667c8b9f96b2d0ec2301d7dbfe8d
kdgreen58 <kolton.green@spsmail.cuny.edu>
2014-11-06 20:51:05 -0500

    My code for task 01

--- task_01.py
+++ task_01.py
@@ -3,9 +3,11 @@
 """My task 01 code."""
 
 GRADE_DATA = {}
-GRADE_DATA['student'] = {'Luke Skywalker','Luke Skywalker','Luke Skywalker',
-                          'Luke Skywalker','Han Solo','Han Solo','Han Solo',
-                          'Han Solo','C-3PO','C-3PO','C-3PO','C-3PO'}
-GRADE_DATA['subject'] = {'math','etiquette','grammer','gym','math','etiquette',
-                         'grammer','gym','math','etiquette','grammer','gym'}
-GRADE_DATA['grade'] = {'B','B+','B','A','A-','C-','B','B','C','A+','A','F'}
+GRADE_DATA['student'] = {'Luke Skywalker', 'Luke Skywalker', 'Luke Skywalker', 
+                          'Luke Skywalker', 'Han Solo','Han Solo', 'Han Solo', 
+                          'Han Solo', 'C-3PO', 'C-3PO', 'C-3PO', 'C-3PO'}
+GRADE_DATA['subject'] = {'math', 'etiquette', 'grammer', 'gym', 'math', 
+                         'etiquette', 'grammer', 'gym', 'math', 'etiquette', 
+                         'grammer', 'gym'}
+GRADE_DATA['grade'] = {'B', 'B+', 'B', 'A', 'A-', 'C-', 'B', 'B', 'C', 'A+', 
+                       'A', 'F'}
