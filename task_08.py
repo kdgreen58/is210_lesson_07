19e0b3203ce37e215c2fdde0c8c7f739a0cc1687
kdgreen58 <kolton.green@spsmail.cuny.edu>
2014-11-11 23:50:02 -0500

    My task 08 code.

--- task_08.py
+++ task_08.py
@@ -4,7 +4,7 @@
 
 from data import FRUIT
 
-SHOPLIST ={
+SHOPLIST = {
     'Comice Pear': 249,
     'Forelle Pear': 299,
     'Red Pear': 249,
@@ -15,8 +15,38 @@ SHOPLIST ={
     'Green Seedless Grapes': 399,
     }
 
-def get_cost_per_item(SHOPLIST):
-    return {k: v * FRUIT[k] for k, v in SHOPLIST.iteritems() if k in FRUIT}
 
-def get_total_cost(SHOPLIST):
-    return sum(get_cost_per_item(SHOPLIST).values())
+def get_cost_per_item(shoplist):
+    """Iterates through the list the return the total cost of each item.
+
+    Args:
+        shoplist(dict): A dictionary of items and their quantity.
+
+    Returns:
+        dict: A dictionary and the total cost for each item. 
+
+    Examples:
+
+        >>>get_cost_per_item(SHOPLIST)
+        {'Forelle Pear': 894.010000001, 'Local Asian Pear': 396.01}
+
+    """
+    return {k: v * FRUIT[k] for k, v in shoplist.iteritems() if k in FRUIT}
+
+
+def get_total_cost(shoplist):
+    """Sums the total cost of all items.
+
+    Args:
+        shoplist(dict): A dictionary of items and their quantity.
+
+    Returns:
+        num(float): Returns the summed total cost of all items in dictionary.
+
+    Examples:
+
+        >>>get_total_cost(SHOPLIST)
+        7902.0800000001
+
+    """
+    return sum(get_cost_per_item(shoplist).values())
