f018a6bf7de5507fd686297a72ed9f5531cdb742
kdgreen58 <kolton.green@spsmail.cuny.edu>
2014-11-11 22:55:36 -0500

    My task 01 code

--- task_01.py
+++ task_01.py
@@ -2,17 +2,23 @@
 # -*- coding: utf-8 -*-
 """My task 01 code."""
 
-GRADE_DATA = {'student':{'Luke Skywalker':{'math':'B',
-                                           'etiquette':'B+',
-                                           'grammar':'B',
-                                           'gym':'A'},
-                         'Han Solo':{'math':'A-',
-                                     'etiquette':'C-',
-                                     'grammar':'B',
-                                     'gym':'B'},
-                         'C-3PO':{'math':'C',
-                                  'etiquette':'A+',
-                                  'grammar':'A',
-                                  'gym':'F'}
-    }
-    }
+GRADE_DATA = {
+    'Luke Skywalker': {
+        'math': 'B',
+        'etiquette': 'B+',
+        'grammar': 'B',
+        'gym': 'A',
+        },
+    'Han Solo': {
+        'math': 'A-',
+        'etiquette': 'C-',
+        'grammar': 'B',
+        'gym': 'B',
+        },
+    'C-3PO': {
+        'math': 'C',
+        'etiquette': 'A+',
+        'grammar': 'A',
+        'gym': 'F',
+        },                        
+}
