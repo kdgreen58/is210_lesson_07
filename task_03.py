71fb7f5c74e88c1d35d9f8930727d50b968dc048
kdgreen58 <kolton.green@spsmail.cuny.edu>
2014-11-11 22:56:34 -0500

    My task 03 code

---
+++ task_03.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Task 03 Code"""
+
+import data
+
+CORRECTED = data.BANDS.copy()
+CORRECTED['Dylan'] = {'Bob Dylan': ['vocals', 'guitar', 'harmonica']}
+del CORRECTED['Van Halen']['David Lee Roth']
+CORRECTED['Van Halen']['Sammy Hagar'] = ['vocals']
